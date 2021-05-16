dddef93b5d24647bd7715d64ce7234681ef1a4e2
Jay_WooKJ <natcho9010@gmail.com>
2021-05-16 16:43:23 +0900

    Add: cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const homeRouter = require("./routes/home");
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", homeRouter);
 
